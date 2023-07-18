47c83dab3f5d772df2ce3f6001676b8d46074bbe
effab <abeffan@gmail.com>
2023-07-18 15:02:08 -0500

    exercise finished

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,10 @@
+export default function HobbyLinks () {
+    const hobbyLinks = ["https://sketch.io/sketchpad/", "https://www.writing.com/"];
+
+    return (
+        <div>
+            <a href={hobbyLinks[0]}>https://sketch.io/sketchpad/</a>
+            <a href={hobbyLinks[1]}>https://www.writing.com/</a>
+        </div>
+    )
+}
