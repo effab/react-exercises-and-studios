861117b65adcf479babafb190011f509f4c3c904
effab <abeffan@gmail.com>
2023-07-21 16:20:36 -0500

    data.json

---
+++ part3/exercises/src/components/Introduction.js
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction() {
+    return (
+        <div>
+            <h2>3 Fun Fact About this Hobby</h2>
+            <ol>
+                <li>Coming up with a character's background is one of the hardest parts of writing a story.</li>
+                <li>There is so much research that a writer has to do before beginning the writitng process.</li>
+                <li>Creative writing is similar to professional athletics.</li>
+            </ol>
+        </div>
+    );
+}
